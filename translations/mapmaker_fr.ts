7f3d7417d39a031c23dd1fe6469d0cc46ef5084c
Kervala <983521+kervala@users.noreply.github.com>
2018-07-25 19:44:46 +0200

    Added: Translations

--- translations/mapmaker_fr.ts
+++ translations/mapmaker_fr.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../ui/logsdialog.ui" line="14"/>
         <source>Logs</source>
-        <translation type="unfinished"></translation>
+        <translation>Journaux</translation>
     </message>
 </context>
 <context>
@@ -14,195 +14,195 @@
     <message>
         <location filename="../ui/mainwindow.ui" line="14"/>
         <source>MapMaker</source>
-        <translation type="unfinished"></translation>
+        <translation>MapMaker</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="37"/>
         <source>Select</source>
-        <translation type="unfinished"></translation>
+        <translation>Sélectionner</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="60"/>
         <location filename="../ui/mainwindow.ui" line="160"/>
         <source>Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Image</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="80"/>
         <location filename="../ui/mainwindow.ui" line="132"/>
         <source>Number</source>
-        <translation type="unfinished"></translation>
+        <translation>Nombre</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="115"/>
         <source>Position</source>
-        <translation type="unfinished"></translation>
+        <translation>Position</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="144"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Police</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="151"/>
         <source>Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Couleur</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="170"/>
         <source>Browse</source>
-        <translation type="unfinished"></translation>
+        <translation>Parcourir</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="202"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Fichier</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="214"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Aide</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="222"/>
         <source>Items</source>
-        <translation type="unfinished"></translation>
+        <translation>Objets</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="233"/>
         <source>&amp;Settings...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Paramètres...</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="241"/>
         <source>E&amp;xit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Quitter</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="249"/>
         <source>Export list to CSV</source>
-        <translation type="unfinished"></translation>
+        <translation>Exporter liste en CSV</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="254"/>
         <source>&amp;About...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;A propos...</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="262"/>
         <source>&amp;Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Supprimer</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="267"/>
         <source>About Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>À propos de Qt</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="278"/>
         <source>&amp;Logs</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Journaux</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="286"/>
         <source>Import images...</source>
-        <translation type="unfinished"></translation>
+        <translation>Importer des images...</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="291"/>
         <source>&amp;Open...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Ouvrir...</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="296"/>
         <source>&amp;Save...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Enregistrer...</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="301"/>
         <source>&amp;Export...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Exporter...</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="306"/>
         <source>&amp;New</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Nouveau</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="311"/>
         <source>Save as...</source>
-        <translation type="unfinished"></translation>
+        <translation>Enregistrer sous...</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="79"/>
         <source>Starting %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Lancement de %1</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="126"/>
         <source>All supported formats (%1)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tous les formats supportés (%1)</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="140"/>
         <source>Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>Confirmation</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="140"/>
         <source>Do you confirm to reset this map?</source>
-        <translation type="unfinished"></translation>
+        <translation>Confirmez vous la réinitialisation de cette carte ?</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="148"/>
         <source>Open project</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir un projet</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="148"/>
         <location filename="../src/mainwindow.cpp" line="170"/>
         <source>Map files (*.kmf)</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichiers de cartes (*.kmf)</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="170"/>
         <source>Save project</source>
-        <translation type="unfinished"></translation>
+        <translation>Enregister le projet</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="180"/>
         <source>Export image</source>
-        <translation type="unfinished"></translation>
+        <translation>Exporter une image</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="203"/>
         <source>About %1 %2 by %3</source>
-        <translation type="unfinished"></translation>
+        <translation>À propos de %1 %2 par %3</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="203"/>
         <source>Map editor</source>
-        <translation type="unfinished"></translation>
+        <translation>Éditeur de cartes</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="228"/>
         <source>Change image</source>
-        <translation type="unfinished"></translation>
+        <translation>Changer l&apos;image</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="242"/>
         <source>Choose font options for numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisir les options de la police pour les nombres</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="256"/>
         <source>Choose color for numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisir la couleur des nombres</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="292"/>
         <source>Import images</source>
-        <translation type="unfinished"></translation>
+        <translation>Importer des images</translation>
     </message>
 </context>
 </TS>
