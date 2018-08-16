2309f73a5fba3182ff04f645956c5f3f85c936c6
Kervala <983521+kervala@users.noreply.github.com>
2018-08-16 12:23:45 +0200

    Changed: Updated translations

--- translations/mapmaker_fr.ts
+++ translations/mapmaker_fr.ts
@@ -2,6 +2,14 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR">
 <context>
+    <name>ColorAction</name>
+    <message>
+        <location filename="../src/coloraction.cpp" line="27"/>
+        <source>Select a color...</source>
+        <translation>Sélectionner une couleur...</translation>
+    </message>
+</context>
+<context>
     <name>LogsDialog</name>
     <message>
         <location filename="../ui/logsdialog.ui" line="14"/>
@@ -17,192 +25,374 @@
         <translation>MapMaker</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="37"/>
+        <location filename="../ui/mainwindow.ui" line="31"/>
+        <source>Select and move tool</source>
+        <translation>Outil de déplacement et sélection</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="34"/>
         <source>Select</source>
         <translation>Sélectionner</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="60"/>
-        <location filename="../ui/mainwindow.ui" line="160"/>
+        <location filename="../ui/mainwindow.ui" line="57"/>
+        <source>Add number tool</source>
+        <translation>Outil d&apos;ajout de nombre</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="80"/>
+        <source>Add symbol tool</source>
+        <translation>Outil d&apos;ajout de symbole</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="83"/>
+        <location filename="../ui/mainwindow.ui" line="341"/>
+        <source>Symbol</source>
+        <translation>Symbole</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="103"/>
+        <source>Zoom tool</source>
+        <translation>Outil de zoom</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="106"/>
+        <source>Zoom</source>
+        <translation>Zoom</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="168"/>
+        <source>x</source>
+        <translation>x</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="178"/>
+        <source>Validate position</source>
+        <translation>Valider position</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="181"/>
+        <location filename="../ui/mainwindow.ui" line="373"/>
+        <location filename="../ui/mainwindow.ui" line="393"/>
+        <location filename="../ui/mainwindow.ui" line="413"/>
+        <location filename="../ui/mainwindow.ui" line="433"/>
+        <location filename="../ui/mainwindow.ui" line="453"/>
+        <location filename="../ui/mainwindow.ui" line="473"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="227"/>
+        <source>Validate number</source>
+        <translation>Valider nombre</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="230"/>
+        <source>Update</source>
+        <translation>Mettre à jour</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="244"/>
+        <source>Recompute all numbers</source>
+        <translation>Recalculer tous les nombres</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="247"/>
+        <source>Recompute</source>
+        <translation>Recalculer</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="261"/>
+        <source>Increment all numbers from this one</source>
+        <translation>Incrémenter tous les nombres suivants</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="264"/>
+        <source>+</source>
+        <translation>+</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="274"/>
+        <source>Change font for numbers</source>
+        <translation>Changer la police pour les nombres</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="277"/>
+        <source>Numbers font</source>
+        <translation>Police des nombres</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="291"/>
+        <location filename="../ui/mainwindow.ui" line="500"/>
+        <source>Change font color for numbers</source>
+        <translation>Changer la couleur de la police pour les nombres</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="294"/>
+        <location filename="../ui/mainwindow.ui" line="503"/>
+        <source>Numbers color</source>
+        <translation>Couleur des nombres</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="350"/>
+        <source>Cross</source>
+        <translation>Croix</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="550"/>
         <source>Image</source>
         <translation>Image</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="80"/>
-        <location filename="../ui/mainwindow.ui" line="132"/>
+        <location filename="../ui/mainwindow.ui" line="562"/>
+        <source>Add/change an image</source>
+        <translation>Ajouter/changer une image</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="565"/>
+        <source>Add or change image</source>
+        <translation>Ajouter ou changer l&apos;image</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="579"/>
+        <location filename="../ui/mainwindow.ui" line="582"/>
+        <source>Origin foreground color</source>
+        <translation>Couleur originale de l&apos;avant-plan</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="596"/>
+        <location filename="../ui/mainwindow.ui" line="599"/>
+        <source>Final foreground color</source>
+        <translation>Couleur finale de l&apos;avant-plan</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="60"/>
+        <location filename="../ui/mainwindow.ui" line="215"/>
         <source>Number</source>
         <translation>Nombre</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="115"/>
+        <location filename="../ui/mainwindow.ui" line="156"/>
         <source>Position</source>
         <translation>Position</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="144"/>
         <source>Font</source>
-        <translation>Police</translation>
+        <translation type="vanished">Police</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="151"/>
         <source>Color</source>
-        <translation>Couleur</translation>
+        <translation type="vanished">Couleur</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="170"/>
         <source>Browse</source>
-        <translation>Parcourir</translation>
+        <translation type="vanished">Parcourir</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="202"/>
+        <location filename="../ui/mainwindow.ui" line="658"/>
         <source>&amp;File</source>
         <translation>&amp;Fichier</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="214"/>
+        <location filename="../ui/mainwindow.ui" line="669"/>
         <source>&amp;Help</source>
         <translation>&amp;Aide</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="222"/>
+        <location filename="../ui/mainwindow.ui" line="677"/>
         <source>Items</source>
         <translation>Objets</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="233"/>
         <source>&amp;Settings...</source>
-        <translation>&amp;Paramètres...</translation>
+        <translation type="vanished">&amp;Paramètres...</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="241"/>
+        <location filename="../ui/mainwindow.ui" line="688"/>
         <source>E&amp;xit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="249"/>
         <source>Export list to CSV</source>
-        <translation>Exporter liste en CSV</translation>
+        <translation type="vanished">Exporter liste en CSV</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="254"/>
+        <location filename="../ui/mainwindow.ui" line="696"/>
         <source>&amp;About...</source>
         <translation>&amp;A propos...</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="262"/>
         <source>&amp;Delete</source>
-        <translation>&amp;Supprimer</translation>
+        <translation type="vanished">&amp;Supprimer</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="267"/>
+        <location filename="../ui/mainwindow.ui" line="704"/>
         <source>About Qt</source>
         <translation>À propos de Qt</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="278"/>
+        <location filename="../ui/mainwindow.ui" line="715"/>
         <source>&amp;Logs</source>
         <translation>&amp;Journaux</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="286"/>
+        <location filename="../ui/mainwindow.ui" line="723"/>
         <source>Import images...</source>
         <translation>Importer des images...</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="291"/>
+        <location filename="../ui/mainwindow.ui" line="728"/>
         <source>&amp;Open...</source>
         <translation>&amp;Ouvrir...</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="296"/>
+        <location filename="../ui/mainwindow.ui" line="733"/>
         <source>&amp;Save...</source>
         <translation>&amp;Enregistrer...</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="301"/>
+        <location filename="../ui/mainwindow.ui" line="738"/>
         <source>&amp;Export...</source>
         <translation>&amp;Exporter...</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="306"/>
+        <location filename="../ui/mainwindow.ui" line="743"/>
         <source>&amp;New</source>
         <translation>&amp;Nouveau</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="311"/>
+        <location filename="../ui/mainwindow.ui" line="748"/>
         <source>Save as...</source>
         <translation>Enregistrer sous...</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="79"/>
+        <location filename="../src/mainwindow.cpp" line="121"/>
         <source>Starting %1</source>
         <translation>Lancement de %1</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="126"/>
+        <location filename="../src/mainwindow.cpp" line="205"/>
         <source>All supported formats (%1)</source>
         <translation>Tous les formats supportés (%1)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="140"/>
+        <location filename="../src/mainwindow.cpp" line="249"/>
         <source>Confirm</source>
         <translation>Confirmation</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="140"/>
+        <location filename="../src/mainwindow.cpp" line="249"/>
         <source>Do you confirm to reset this map?</source>
         <translation>Confirmez vous la réinitialisation de cette carte ?</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="148"/>
+        <location filename="../src/mainwindow.cpp" line="262"/>
         <source>Open project</source>
         <translation>Ouvrir un projet</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="148"/>
-        <location filename="../src/mainwindow.cpp" line="170"/>
+        <location filename="../src/mainwindow.cpp" line="262"/>
+        <location filename="../src/mainwindow.cpp" line="296"/>
         <source>Map files (*.kmf)</source>
         <translation>Fichiers de cartes (*.kmf)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="170"/>
+        <location filename="../src/mainwindow.cpp" line="269"/>
+        <source>Unable to load %1</source>
+        <translation>Impossible de charger %1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="296"/>
         <source>Save project</source>
         <translation>Enregister le projet</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="180"/>
+        <location filename="../src/mainwindow.cpp" line="303"/>
+        <source>Unable to save %1</source>
+        <translation>Impossible d&apos;enregistrer %1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="315"/>
         <source>Export image</source>
         <translation>Exporter une image</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="203"/>
+        <location filename="../src/mainwindow.cpp" line="315"/>
+        <source>SVG file (*.svg)</source>
+        <translation>Fichier SVG (*.svg)</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="315"/>
+        <source>PDF file (*.pdf)</source>
+        <translation>Fichier PDF (*.pdf)</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="322"/>
+        <source>Unable to export map to image %1</source>
+        <translation>Impossible d&apos;exporter la carte en image %1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="338"/>
         <source>About %1 %2 by %3</source>
         <translation>À propos de %1 %2 par %3</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="203"/>
+        <location filename="../src/mainwindow.cpp" line="338"/>
         <source>Map editor</source>
         <translation>Éditeur de cartes</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="228"/>
+        <location filename="../src/mainwindow.cpp" line="395"/>
         <source>Change image</source>
         <translation>Changer l&apos;image</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="242"/>
+        <location filename="../src/mainwindow.cpp" line="404"/>
+        <source>Unable to open image %1</source>
+        <translation>Impossible d&apos;ouvrir l&apos;image %1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="438"/>
         <source>Choose font options for numbers</source>
         <translation>Choisir les options de la police pour les nombres</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="256"/>
+        <location filename="../src/mainwindow.cpp" line="452"/>
         <source>Choose color for numbers</source>
         <translation>Choisir la couleur des nombres</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="292"/>
+        <location filename="../src/mainwindow.cpp" line="468"/>
+        <source>Choose color for origin foreground</source>
+        <translation>Choisir une couleur pour l&apos;avant-plan original</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="484"/>
+        <source>Choose color for final foreground</source>
+        <translation>Choisir une couleur pour l&apos;avant-plan final</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="513"/>
+        <source>Choose color for symbols</source>
+        <translation>Choisir une couleur pour les symboles</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="641"/>
         <source>Import images</source>
         <translation>Importer des images</translation>
+    </message>
+</context>
+<context>
+    <name>MapScene</name>
+    <message>
+        <location filename="../src/mapscene.cpp" line="615"/>
+        <source>MapMaker SVG Generator</source>
+        <translation>MapMaker SVG Generator</translation>
+    </message>
+    <message>
+        <location filename="../src/mapscene.cpp" line="616"/>
+        <source>A MapMaker map exported as SVG</source>
+        <translation>A MapMaker map exported as SVG</translation>
     </message>
 </context>
 </TS>
